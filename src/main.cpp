571d20cee329f62853dc791eee7bc06803fd9f80
Fedosov Alexander <achpile@gmail.com>
2013-07-17 14:53:23 +0400

    Window initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	return 0;
+	// After take the settings from config file (resolution and fullscreen)
+	sf::RenderWindow App(sf::VideoMode(1024, 600, 32), "Name get from define", sf::Style::Fullscreen);
+
+	while(App.IsOpened()) {
+		App.Clear();
+		App.Display();
+	}
+
+	return EXIT_SUCCESS;
 }

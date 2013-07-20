83afd3cd182f0ac24c7399193198c5edcdab35f8
Fedosov Alexander <achpile@gmail.com>
2013-07-20 14:15:59 +0400

    Added menu and submenu vectors & rendering menu caption

--- src/menu/menuitem.cpp
+++ src/menu/menuitem.cpp
@@ -1,11 +1,12 @@
 #include "game.hpp"
 
-void fired::MenuItem::init(fired::Game *_game, sf::Sprite *_sprite, sf::Font *_font, const char *_caption) {
+void fired::MenuItem::init(fired::Game *_game, sf::Sprite *_sprite, sf::Font *_font, const char *_caption, fired::MenuItem *_parent) {
 	sf::FloatRect textRect;
 
 	game    = _game;
 	font    = _font;
 	sprite  = _sprite;
+	parent  = _parent;
 	caption = sf::String(_caption);
 
 	text.setFont(*font);
@@ -18,8 +19,14 @@ void fired::MenuItem::init(fired::Game *_game, sf::Sprite *_sprite, sf::Font *_f
 }
 
 
-void fired::MenuItem::setIndex(int index) {
-	pos = sf::Vector2f(50, 200 + 75*index);
+void fired::MenuItem::addToParent() {
+	if (parent) parent->addSubMenu(this);
+}
+
+
+void fired::MenuItem::addSubMenu(fired::MenuItem *subMenuItem) {
+	subMenu.push_back(subMenuItem);
+	subMenuItem->pos = sf::Vector2f(50, 150 + 75 * subMenu.size());
 }
 
 

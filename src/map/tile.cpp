98e262a583cdaa5ac8501cae5800ec55e9f461a3
Fedosov Alexander <achpile@gmail.com>
2014-01-12 10:38:59 +0400

    Make wall tiles darker

--- src/map/tile.cpp
+++ src/map/tile.cpp
@@ -10,7 +10,7 @@ void fired::Tile::set(int _tileset, bool _isWall, bool _isPlatform, int x, int y
 	pos        = sf::Vector2f(x * TILE_SIZE, y * TILE_SIZE);
 
 	if (isWall) color = sf::Color::White;
-	else        color = sf::Color(180, 180, 180, 255);
+	else        color = sf::Color(140, 140, 140, 255);
 }
 
 //======================================================================

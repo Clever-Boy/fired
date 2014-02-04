e70c18dc1bf566a870ac75ce2c0ec95987691a51
Fedosov Alexander <achpile@gmail.com>
2014-02-04 13:41:45 +0400

    Moved out physics from themap.cpp file

--- include/map/themap.hpp
+++ include/map/themap.hpp
@@ -56,18 +56,13 @@ namespace fired {
 		void render();
 		void spawn();
 
-		bool isSolid(int i, int j);
-		bool isPlatform(int i, int j, fired::Phys *phys);
 		void findTile(int i, int j);
 		void findTiles(int x1, int y1, int x2, int y2) {for (int i = x1; i < x2; i++) for (int j = y1; j < y2; j++) findTile(i, j); };
-
-		void checkCollision(fired::Phys *phys, fired::Character *character, int tile_x, int tile_y);
-		void checkPhys(fired::Phys *phys, fired::Character *character, float tunnelTime);
-		bool checkShot(fired::Shot *shot);
 
 		fired::MapObject *checkInteraction(fired::Character *owner);
 
 		#include "lighting.hpp"
+		#include "physics.hpp"
 	};
 }
 

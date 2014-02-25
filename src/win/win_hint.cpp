99a0528b3d9f38106a7124e625befdf1df330b9a
Fedosov Alexander <achpile@gmail.com>
2014-02-25 16:52:04 +0400

    Weapon SubType in hint window

--- src/win/win_hint.cpp
+++ src/win/win_hint.cpp
@@ -81,71 +81,89 @@ void fired::HintWindow::render(fired::BaseItem *item) {
 
 
 	if (item->type == itAny) {
-		win->setSize(sf::Vector2f(150, 40));
+		win->setSize(sf::Vector2f(200, 40));
 		win->render();
 
 		renderText(5, 5, "Misc", false);
 	} else if (item->type == itWeapon) {
 		fired::BaseWeapon *base = container->weapons[item->UID];
 
-		if (base->type == WEAPON_TYPE_RANGED) win->setSize(sf::Vector2f(150, 145));
-		else                                  win->setSize(sf::Vector2f(150, 115));
+		if (base->explosive)                       win->setSize(sf::Vector2f(200, 160));
+		else if (base->type == WEAPON_TYPE_RANGED) win->setSize(sf::Vector2f(200, 145));
+		else                                       win->setSize(sf::Vector2f(200, 115));
 		win->render();
 
 		renderText(5, 5, base->caption, false);
 		renderText(5, 20, "Weapon class:", false);
+		renderText(5, 35, "Weapon subclass:", false);
 
 		switch (base->type) {
-			case WEAPON_TYPE_RANGED: renderText(145, 20, "ranged", true); break;
-			case WEAPON_TYPE_MELEE : renderText(145, 20, "melee", true); break;
-			case WEAPON_TYPE_BROAD : renderText(145, 20, "broad", true); break;
+			case WEAPON_TYPE_RANGED: renderText(195, 20, "ranged", true); break;
+			case WEAPON_TYPE_MELEE : renderText(195, 20, "melee", true); break;
+			case WEAPON_TYPE_BROAD : renderText(195, 20, "broad", true); break;
 		}
 
-		if (base->automatic) renderText(5, 35, "Automatic", false);
-		else                 renderText(5, 35, "Non-automatic", false);
+		switch (base->subtype) {
+			case WEAPON_SUBTYPE_MELEE     : renderText(195, 35, "melee"    , true); break;
+			case WEAPON_SUBTYPE_BROAD     : renderText(195, 35, "broad"    , true); break;
+			case WEAPON_SUBTYPE_PISTOL    : renderText(195, 35, "pistol"   , true); break;
+			case WEAPON_SUBTYPE_SHOTGUN   : renderText(195, 35, "shotgun"  , true); break;
+			case WEAPON_SUBTYPE_RIFLE     : renderText(195, 35, "rifle"    , true); break;
+			case WEAPON_SUBTYPE_ENERGY    : renderText(195, 35, "energy"   , true); break;
+			case WEAPON_SUBTYPE_EXPLOSIVE : renderText(195, 35, "explosive", true); break;
+		}
 
-		renderText(5, 50, "Damage", false);
+		if (base->automatic) renderText(5, 50, "Automatic", false);
+		else                 renderText(5, 50, "Non-automatic", false);
+
+		renderText(5, 65, "Damage", false);
 		snprintf(str, sizeof(str), "%d", base->damage);
-		renderText(145, 50, str, true);
+		renderText(195, 65, str, true);
 
-		renderText(5, 65, "Knockback", false);
+		renderText(5, 80, "Knockback", false);
 		snprintf(str, sizeof(str), "%3.2f", base->knockback);
-		renderText(145, 65, str, true);
+		renderText(195, 80, str, true);
 
-		renderText(5, 80, "Range", false);
+		renderText(5, 95, "Range", false);
 		snprintf(str, sizeof(str), "%3.2f", base->range);
-		renderText(145, 80, str, true);
+		renderText(195, 95, str, true);
 
-		renderText(5, 95, "Cooldown", false);
+		renderText(5, 110, "Cooldown", false);
 		snprintf(str, sizeof(str), "%3.2f", base->cooldown);
-		renderText(145, 95, str, true);
+		renderText(195, 110, str, true);
 
 		if (base->type == WEAPON_TYPE_RANGED) {
 			renderText(5, 125, "Speed", false);
 			snprintf(str, sizeof(str), "%3.2f", base->speed);
-			renderText(145, 125, str, true);
+			renderText(195, 125, str, true);
+		}
+
+		if (base->explosive) {
+			renderText(5, 140, "Explosion radius", false);
+			snprintf(str, sizeof(str), "%3.2f", base->explosionRadius);
+			renderText(195, 140, str, true);
 		}
 	} else {
 		fired::BaseArmor *base = container->armors[item->UID];
 
-		win->setSize(sf::Vector2f(150, 55));
+		win->setSize(sf::Vector2f(200, 55));
 		win->render();
 
 		renderText(5, 5, base->caption, false);
 		renderText(5, 20, "Armor class:", false);
 
 		switch (base->type) {
-			case acHelm: renderText(145, 20, "helm" , true); break;
-			case acFist: renderText(145, 20, "fist" , true); break;
-			case acBody: renderText(145, 20, "torso", true); break;
-			case acLegs: renderText(145, 20, "legs" , true); break;
-			case acShoe: renderText(145, 20, "shoes", true); break;
-			case acArms: renderText(145, 20, "arms" , true); break;
+			case acHelm: renderText(195, 20, "helm" , true); break;
+			case acFist: renderText(195, 20, "fist" , true); break;
+			case acBody: renderText(195, 20, "torso", true); break;
+			case acLegs: renderText(195, 20, "legs" , true); break;
+			case acShoe: renderText(195, 20, "shoes", true); break;
+			case acArms: renderText(195, 20, "arms" , true); break;
 			case acNull: break;
 		}
 
 		renderText(5, 35, "Armor:", false);
 		snprintf(str, sizeof(str), "%d", base->armor);
-		renderText(145, 35, str, true);
+		renderText(195, 35, str, true);
 	}
 }
